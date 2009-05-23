4282166b61187d7bdb480c2d9e98b6bed8e3908a
Parashuram N <github@nparashuram.com>
2009-05-23 08:04:10 +0000

    Added build script and changed to URLConnection from HttpClient for twittering.

--- src/com/twitter/TwitBot.java
+++ src/com/twitter/TwitBot.java
@@ -1,21 +1,16 @@
 package com.twitter;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+
 import java.util.List;
 
-import org.apache.commons.httpclient.Credentials;
-import org.apache.commons.httpclient.HostConfiguration;
-import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.NameValuePair;
-import org.apache.commons.httpclient.SimpleHttpConnectionManager;
-import org.apache.commons.httpclient.URI;
 import org.apache.commons.httpclient.URIException;
-import org.apache.commons.httpclient.UsernamePasswordCredentials;
-import org.apache.commons.httpclient.auth.AuthScope;
-import org.apache.commons.httpclient.methods.PostMethod;
 
 import com.sun.syndication.feed.synd.SyndEntryImpl;
 import com.sun.syndication.feed.synd.SyndFeed;
@@ -25,38 +20,26 @@ import com.sun.syndication.io.XmlReader;
 
 public class TwitBot {
 
-	private static String twitterhost = "www.twitter.com";
+	private String twitterhost = "http://www.twitter.com";
+	private String login = "twitteybot";
+	private String password = "Welcome1";
 
-	public static void main(String args[]) throws Exception {
-
-		while (true) {
-			String feed = getFeed();
-			System.out.println(feed);
-			if (feed != null)
-				twitter(feed);
-			Thread.sleep(1000*120);
-		}
+	public void twitter(String status) throws URIException, IOException,
+			HttpException {
+		String statusURL = "/statuses/update.xml";
+		String userPassword = login + ":" + password;
+		String encoding = new String(new Base64().encode(userPassword.getBytes()));
+		URL url = new URL(twitterhost + statusURL);
+		HttpURLConnection openConnection = (HttpURLConnection) url.openConnection();
+		openConnection.setRequestProperty ("Authorization", "Basic " + encoding);
+		openConnection.setDoOutput(true);
+		openConnection.setRequestMethod("POST");
+		OutputStreamWriter writer = new OutputStreamWriter(openConnection.getOutputStream());
+        writer.write("status=" + status);
+        writer.close();
 	}
 
-	private static void twitter(String status) throws URIException,
-			IOException, HttpException {
-		HttpClient client = new HttpClient(new SimpleHttpConnectionManager());
-		String url = "/statuses/update.xml";
-		PostMethod method = new PostMethod(url);
-		Credentials credentials = new UsernamePasswordCredentials("twitteybot",
-				"Welcome1");
-		client.getState().setCredentials(
-				new AuthScope(twitterhost, 80, AuthScope.ANY_REALM),
-				credentials);
-		NameValuePair params[] = new NameValuePair[1];
-		params[0] = new NameValuePair("status", status);
-		method.setRequestBody(params);
-		HostConfiguration host = client.getHostConfiguration();
-		host.setHost(new URI("http://" + twitterhost, true));
-		client.executeMethod(host, method);
-	}
-
-	private static String getFeed() throws FeedException, IOException,
+	public String getFeed() throws FeedException, IOException,
 			MalformedURLException {
 		SyndFeedInput input = new SyndFeedInput();
 		SyndFeed feed = input.build(new XmlReader(new URL(
