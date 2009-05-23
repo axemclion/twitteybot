2c41beacd8bf0e0a221b814003c2526617b59e47
news4nishant <news4nishant@localhost>
2009-05-23 08:07:57 +0000

    Added build script and admin servlet

---
+++ src/com/twitter/AdminServlet.java
@@ -0,0 +1,26 @@
+package com.twitter;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AdminServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		this.doPost(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		TwitBot bot = new TwitBot();
+		bot.twitter("Just testing");
+		resp.getWriter().write("tweet successful");
+	}
+
+}
