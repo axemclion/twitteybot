698aaa14830299d3303ea9f11ef94334deda6c69
n.parashuram <n.parashuram@localhost>
2009-05-31 12:48:12 +0000

    Changed project structure, added stubs for classes

---
+++ src/com/appspot/twitteybot/datastore/Settings.java
@@ -0,0 +1,64 @@
+package com.appspot.twitteybot.datastore;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.users.User;
+
+/**
+ * Class that holds the setting of a specific user
+ */
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class Settings {
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Long id;
+
+    @Persistent
+    private User user;
+
+    @Persistent
+    private List<TwitterSettings> twitterAccounts;
+
+    @Persistent
+    private Date dateCreated;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<TwitterSettings> getTwitterAccounts() {
+        return twitterAccounts;
+    }
+
+    public void setTwitterAccounts(List<TwitterSettings> twitterAccounts) {
+        this.twitterAccounts = twitterAccounts;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+}
---
+++ war/index.jsp
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>
+			TwitteyBot :: A Bots and bot management application for Twitter
+		</title>
+	</head>
+	<body>
+	</body>
+</html>
