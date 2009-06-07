faecbc5fd33c068417c0912db5b0c593309df506
n.parashuram <n.parashuram@localhost>
2009-06-07 09:04:44 +0000

    Added more functionality to FeedCron, not working yet

--- src/com/appspot/twitteybot/feeds/FeedCron.java
+++ src/com/appspot/twitteybot/feeds/FeedCron.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -14,33 +15,33 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.appspot.twitteybot.datastore.FeedConfiguration;
 
-public class FeedCron
-    extends HttpServlet {
+public class FeedCron extends HttpServlet {
     private static final long serialVersionUID = 3266332204576717233L;
     private static final Logger log = Logger.getLogger(FeedCron.class.getName());
 
     /**
-     * Will be called every minute by the Cron Job on the Google app engine {@inheritDoc}
+     * Will be called every minute by the Cron Job on the Google app engine
+     * {@inheritDoc}
      */
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
-        IOException {
-        FeedReader reader = new FeedReader();
-        List<URL> activeFeedURLs = new ArrayList<URL>();
-        Calendar currentTime = Calendar.getInstance();
-        
-        List<FeedConfiguration> configs = reader.getFeedUrls();
-        for (FeedConfiguration config : configs) {
-            Calendar cal = config.getNextUpdate();
-            if (Math.round(cal.getTimeInMillis() / 1000) == Math.round(currentTime
-                .getTimeInMillis() / 1000)) {
-                activeFeedURLs.add(config.getFeedUrl());
-                config.setPreviousUpdate(currentTime);
-                currentTime.add(Calendar.MINUTE, config.getFeedUpdateInterval());
-                config.setNextUpdate(currentTime);
-            }
-        }
-        reader.updateCache(configs);
-        reader.saveFeeds(activeFeedURLs);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	FeedReader reader = new FeedReader();
+	List<URL> activeFeedURLs = new ArrayList<URL>();
+	Calendar currentTime = Calendar.getInstance();
+
+	List<FeedConfiguration> configs = reader.getFeedUrls();
+	for (FeedConfiguration config : configs) {
+	    Calendar cal = Calendar.getInstance();
+	    cal.setTime(config.getNextUpdate());
+	    if (Math.round(cal.getTimeInMillis() / 1000) == Math.round(currentTime.getTimeInMillis() / 1000)) {
+		log.log(Level.FINE, "Reading Log", config.getFeedUrl());
+		activeFeedURLs.add(new URL(config.getFeedUrl()));
+		config.setPreviousUpdate(currentTime.getTime());
+		currentTime.add(Calendar.MINUTE, config.getFeedUpdateInterval());
+		config.setNextUpdate(currentTime.getTime());
+	    }
+	}
+	reader.updateCache(configs);
+	reader.saveFeeds(activeFeedURLs);
     }
 }
