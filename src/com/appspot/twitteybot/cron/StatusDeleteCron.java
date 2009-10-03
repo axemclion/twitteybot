4c5aacf0555434326660b80c4edc6a53f424c381
Parashuram N <github@nparashuram.com>
2009-10-03 09:26:02 +0000

    Deleted unnecessary imports, removed warnings

--- src/com/appspot/twitteybot/cron/StatusDeleteCron.java
+++ src/com/appspot/twitteybot/cron/StatusDeleteCron.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class StatusDeleteCron extends HttpServlet {
+
+	private static final long serialVersionUID = -1578502604462452971L;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
 			IOException {
