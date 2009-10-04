7899749b0c66072469228cc5f80be3b8226ef137
n.parashuram <n.parashuram@localhost>
2009-10-04 05:02:08 +0000

    Changed Tweets layout

--- web/js/TwitteyBot.js
+++ web/js/TwitteyBot.js
@@ -42,208 +42,219 @@ $(document).ready(function(){
     
     });
     
-    var TwitteyBot = {
-        dateFormat: "dddd, MMMM dd, yyyy, hh:mm:ss tt",
-        init: function(){
-            var me = this;
-            if ($("#twitterAccountList>li").size() == 0) {
-                $("#twitterAccount").hide();
-                $("#scheduler").hide();
-                this.showMessage("Add a twitter account to tweet scheduled messages to it", "warn", true);
-            }
-            this.initTwitterAcccounts();
-            this.initTwitterStatusActions();
-            this.initScheduler();
-            
-            $(".actionBar>.right-Pane>div").hide();
-            $("textarea,input[type=text]").bind("focus", function(){
-                this.select();
-            });
-            $("#twitterAccountList a:first").click();
-            window.setTimeout(this.updateVisibleTimes, 2000);
-        },
-        
-        initTwitterAcccounts: function(){
-            var me = this;
-            $("#actionList>li>a").click(function(event){
-                $(".actionBar>.right-Pane>*").hide();
-                $(this.href.substring(this.href.indexOf("#"))).fadeIn();
-                return false;
-            });
-            $(".actionBar :reset, :submit").click(function(){
-                $(".actionBar>.right-Pane>*").hide();
-                $(".actionBar>.right-Pane>ul").fadeIn();
-            });
-            $("#twitterAccountList a").click(function(event){
-                var screenName = $.urlParser(this.href).params["screenName"];
-                $("#twitterScreenName").html(screenName);
-                $("#screenName").attr("value", screenName);
-                me.showTweets();
-                return false;
-            });
-            
-            $("#uploadFileForm form").submit(function(){
-                $(this).attr("action", "/pages/status?action=Upload&screenName=" + $("#twitterScreenName").html());
-                $("#uploadButtons").show();
-                $("#otherButtons").hide();
-            });
-            $("#deleteAccountForm form").submit(function(){
-                $.get("/pages/manageTwitterAccount", {
-                    "action": "delete",
-                    "screenName": $("#twitterScreenName").html()
-                }, function(data, status){
-                    if (status !== "success") {
-                        me.showMessage("Could not delete account " + $("#twitterScreenName").html(), "error");
-                    }
-                    else {
-                        me.showMessage($("#twitterScreenName").html() + " deleted successfully. Refreshing accounts");
-                        window.location = window.location.href;
-                    }
-                });
-                return false;
-            });
-        },
-        
-        initTwitterStatusActions: function(){
-            var me = this;
-            $("#selectNoneStatus").click(function(){
-                $("#twitterStatus .item-index").attr("checked", false);
-                return false;
-            });
-            $("#selectAllStatus").click(function(){
-                $("#twitterStatus .item-index").attr("checked", true);
-                return false;
-            });
-            $("#twitterContent :reset").click(this.showTweets);
-            
-            $("#resultFrame").load(function(){
-                $("#twitterStatus").html(this.contentDocument.body.innerHTML);
-                me.onTweetsLoaded();
-                me.showMessage($("#responseMessage").attr("value"), $("#responseMessage").attr("title"));
-            });
-            
-            
-            $("#twitterContent form").submit(function(){
-                $("#uploadButtons").hide();
-                $("#otherButtons").show();
-            });
-        },
-        
-        initScheduler: function(){
-            var me = this;
-            $("#scheduleStart a").click(function(){
-                $("#scheduleStart *").toggle();
-                $("#scheduleStart input[type=text]").focus();
-                return false;
-            });
-            $("#scheduleStart input[type=text]").blur(function(){
-                var startDate = Date.parse(this.value);
-                if (startDate !== null) {
-                    $("#scheduleStart *").toggle();
-                    $("#scheduleStart a").html(startDate.toString(TwitteyBot.dateFormat));
-                    $(this).css("border", "");
-                }
-                else {
-                    $(this).css("border", "SOLID 1px RED");
-                }
-            });
-            
-            $("#scheduler table input[type=text]").blur(function(){
-                var duration = $.parseInterval(this.value);
-                if (this.duration == 0) {
-                    $(this).css("border", "SOLID RED 1px");
-                }
-                else {
-                    $(this).attr("value", duration + " minutes").css("border", "")
-                }
-            });
-            
-            $("#scheduler form").submit(function(){
-                $("#scheduler input[type=text]").blur();
-                var total = parseInt($("#totalItems").val(), 10) + 1;
-                var interval = 0;
-                if (nextTime === null) {
-                    me.showMessage("Starting time is incorrect", "error");
-                    return;
-                }
-                
-                if ($("#scheduler input:checked").val() === "scheduleInterval") {
-                    interval = parseInt($("#scheduleInterval").val());
-                }
-                else {
-                    interval = parseInt($("#scheduleSpan").val(), 10) / (total);
-                }
-                
-                var nextTime = Date.parse($("#scheduleStart input[type=text]").val());
-                //nextTime.addMinutes(-1 * nextTime.getTimezoneOffset());
-                
-                for (var i = 0; i < total; i++) {
-                    if ($("#item_" + i).val() === "on") {
-                        $("#updatedTime_" + i).val(nextTime.getTime());
-                        nextTime.addMinutes(interval);
-                    }
-                }
-                me.updateVisibleTimes()
-                return false;
-            });
-        },
-        
-        updateVisibleTimes: function(){
-            $("#twitterStatus .actual-time").each(function(){
-                var date = new Date();
-                date.setTime($(this).val());
-                //date.addMinutes(date.getTimezoneOffset());
-                var identifier = $(this).attr("id").split("_")[1];
-                $("#date_" + identifier).val(date.toString("dddd, MMMM dd, yyyy"));
-                $("#time_" + identifier).val(date.toString("hh:mm:ss tt"));
-            });
-        },
-        
-        showTweets: function(){
-            var screenName = $("#twitterScreenName").html();
-            var me = this;
-            $("#twitterStatus").load("/pages/status", {
-                "action": "show",
-                "screenName": screenName
-            }, me.onTweetsLoaded);
-            $("#uploadButtons").hide();
-            $("#otherButtons").show();
-        },
-        
-        onTweetsLoaded: function(){
-            if ($("#twitterContent .tweetLine").size() === 0) {
-                $("#twitterContent").hide();
-                $("#noTweets").show();
-            }
-            else {
-                $("#noTweets").hide();
-                $("#twitterContent").show();
-				this.updateVisibleTimes();
-            }
-        },
-        
-        showMessage: function(message, level, dontFade){
-            if (!message || message.trim() == "") {
-                return;
-            }
-            if (!level) {
-                level = "info";
-            }
-            var color = {
-                "info": "GREEN",
-                "error": "RED",
-                "warn": "YELLOW"
-            };
-            $("#message").html(message);
-            $("#message").css("backgroundColor", color[level]);
-            $("#message").fadeIn();
-            if (!dontFade) {
-                window.setTimeout(function(){
-                    $("#message").fadeOut();
-                }, 5000);
-            }
-        },
-    };
-    
     TwitteyBot.init();
 });
+
+var TwitteyBot = {
+    dateFormat: "dddd, MMMM dd, yyyy, hh:mm:ss tt",
+    init: function(){
+        var me = this;
+        if ($("#twitterAccountList>li").size() == 0) {
+            $("#twitterAccount").hide();
+            $("#scheduler").hide();
+            this.showMessage("Add a twitter account to tweet scheduled messages to it", "warn", true);
+        }
+        this.initTwitterAcccounts();
+        this.initTwitterStatusActions();
+        this.initScheduler();
+        
+        $(".actionBar>.right-Pane>div").hide();
+        $("textarea,input[type=text]").bind("focus", function(){
+            this.select();
+        });
+        $("#twitterAccountList a:first").click();
+    },
+    
+    initTwitterAcccounts: function(){
+        var me = this;
+        $("#actionList>li>a").click(function(event){
+            $(".actionBar>.right-Pane>*").hide();
+            $(this.href.substring(this.href.indexOf("#"))).fadeIn();
+            return false;
+        });
+        $(".actionBar :reset, :submit").click(function(){
+            $(".actionBar>.right-Pane>*").hide();
+            $(".actionBar>.right-Pane>ul").fadeIn();
+        });
+        $("#twitterAccountList a").click(function(event){
+            var screenName = $.urlParser(this.href).params["screenName"];
+            $("#twitterScreenName").html(screenName);
+            $("#screenName").attr("value", screenName);
+            me.showTweets(true);
+            return false;
+        });
+        
+        $("#uploadFileForm form").submit(function(){
+            $(this).attr("action", "/pages/status?action=Upload&screenName=" + $("#twitterScreenName").html());
+            $("#uploadButtons").show();
+            $("#otherButtons").hide();
+        });
+        $("#deleteAccountForm form").submit(function(){
+            $.get("/pages/manageTwitterAccount", {
+                "action": "delete",
+                "screenName": $("#twitterScreenName").html()
+            }, function(data, status){
+                if (status !== "success") {
+                    me.showMessage("Could not delete account " + $("#twitterScreenName").html(), "error");
+                }
+                else {
+                    me.showMessage($("#twitterScreenName").html() + " deleted successfully. Refreshing accounts");
+                    window.location = window.location.href;
+                }
+            });
+            return false;
+        });
+    },
+    
+    initTwitterStatusActions: function(){
+        var me = this;
+        $("#selectNoneStatus").click(function(){
+            $("#twitterStatus .item-index").attr("checked", false);
+            return false;
+        });
+        $("#selectAllStatus").click(function(){
+            $("#twitterStatus .item-index").attr("checked", true);
+            return false;
+        });
+        $("#twitterContent :reset").click(this.showTweets);
+        
+        $("#resultFrame").load(function(){
+            $("#twitterStatus").html(this.contentDocument.body.innerHTML);
+            me.onTweetsLoaded(true);
+            me.showMessage($("#responseMessage").attr("value"), $("#responseMessage").attr("title"));
+        });
+        
+        $("#twitterContent form").submit(function(){
+            $("#uploadButtons").hide();
+            $("#otherButtons").show();
+        });
+    },
+    
+    initScheduler: function(){
+        var me = this;
+        $("#scheduleStart a").click(function(){
+            $("#scheduleStart *").toggle();
+            $("#scheduleStart input[type=text]").focus();
+            return false;
+        });
+        $("#scheduleStart input[type=text]").blur(function(){
+            var startDate = Date.parse(this.value);
+            if (startDate !== null) {
+                $("#scheduleStart *").toggle();
+                $("#scheduleStart a").html(startDate.toString(TwitteyBot.dateFormat));
+                $(this).css("border", "");
+            }
+            else {
+                $(this).css("border", "SOLID 1px RED");
+            }
+        });
+        
+        $("#scheduler table input[type=text]").blur(function(){
+            var duration = $.parseInterval(this.value);
+            if (this.duration == 0) {
+                $(this).css("border", "SOLID RED 1px");
+            }
+            else {
+                $(this).attr("value", duration + " minutes").css("border", "")
+            }
+        });
+        
+        $("#scheduler form").submit(function(){
+            $("#scheduler input[type=text]").blur();
+            var total = parseInt($("#totalItems").val(), 10) + 1;
+            var interval = 0;
+            if (nextTime === null) {
+                me.showMessage("Starting time is incorrect", "error");
+                return;
+            }
+            if ($("#scheduler input:checked").val() === "scheduleInterval") {
+                interval = parseInt($("#scheduleInterval").val());
+            }
+            else {
+                interval = parseInt($("#scheduleSpan").val(), 10) / (total);
+            }
+            
+            var nextTime = Date.parse($("#scheduleStart input[type=text]").val());
+            for (var i = 0; i < total; i++) {
+                if ($("#item_" + i).attr("checked") === true) {
+                    $("#updatedTime_" + i).val(nextTime.getTime());
+                    nextTime.addMinutes(interval);
+                }
+            }
+            me.updateVisibleTimes()
+            return false;
+        });
+    },
+    
+    updateVisibleTimes: function(){
+        $("#twitterStatus .actual-time").each(function(){
+            var date = new Date();
+            date.setTime($(this).val());
+            var identifier = $(this).attr("id").split("_")[1];
+            $("#time_" + identifier).val(date.toString(TwitteyBot.dateFormat));
+        });
+    },
+    
+    showTweets: function(){
+        var screenName = $("#twitterScreenName").html();
+        var me = this;
+        $("#twitterStatus").load("/pages/status", {
+            "action": "show",
+            "screenName": screenName
+        }, me.onTweetsLoaded);
+        $("#uploadButtons").hide();
+        $("#otherButtons").show();
+    },
+    
+    onTweetsLoaded: function(caller){
+        if ($("#twitterContent .tweetLine").size() === 0) {
+            $("#twitterContent").hide();
+            $("#noTweets").show();
+        }
+        else {
+            $("#noTweets").hide();
+            $("#twitterContent").show();
+        }
+        
+        $(".tweetLine .time").blur(function(){
+            var date = Date.parse($(this).val());
+            if (date !== null) {
+                $(this).val(date.toString(TwitteyBot.dateFormat));
+                $(this).removeClass("focus-time");
+                var identifier = $(this).attr("id").split("_")[1];
+                $("#updatedTime_" + identifier).val(date.getTime());
+                $("#item_" + identifier).attr("checked", true);
+            }
+            else {
+                $(this).css("color", "red");
+            }
+        }).focus(function(){
+            $(this).addClass("focus-time");
+            $(this).select();
+        });
+        
+        TwitteyBot.updateVisibleTimes();
+    },
+    
+    showMessage: function(message, level, dontFade){
+        if (!message || message.trim() == "") {
+            return;
+        }
+        if (!level) {
+            level = "info";
+        }
+        var color = {
+            "info": "GREEN",
+            "error": "RED",
+            "warn": "YELLOW"
+        };
+        $("#message").html(message);
+        $("#message").css("backgroundColor", color[level]);
+        $("#message").fadeIn();
+        if (!dontFade) {
+            window.setTimeout(function(){
+                $("#message").fadeOut();
+            }, 5000);
+        }
+    },
+};
--- web/templates/ShowStatus.ftl
+++ web/templates/ShowStatus.ftl
@@ -1,31 +1,37 @@
-<table cellpadding = "0" cellspacing = 0>
-    <tr>
-        <th>
-            &nbsp;
-        </th>
-        <th>
-            Time
-        </th>
-        <th>
-            Tweet
-        </th>
-    </tr>
-    <#assign totalItems = "0"><#list statuses as item><#assign totalItems = item_index>
-    <tr class = "tweetLine">
-        <td>
-            <input type = "checkbox" id = "item_${item_index}" name = "item_${item_index}" checked = "true" class = "item-index" value = "on"/><input type = "hidden" name = "key_${item_index}" value = "${item.keyId!""}"/><input type = "hidden" name = "source_${item_index}" value = "${item.source}"/>
-        </td>
-        <td>
-            <input type = "text" class = "update-date" id = "date_${item_index}" value = "${item.updatedTime?string("EEEE, MMMM dd, yyyy")}"/>
-            <br/>
-            <input type = "text" class = "update-time" id = "time_${item_index}" value = "${item.updatedTime?string("hh:mm:ss a")}"/>
-            <br/>
-            <input type = "text" class = "actual-time" id = "updatedTime_${item_index}" name = "updatedTime_${item_index}" class = "text-box" value = "${item.time}"/>
-        </td>
-        <td>
-            <textarea id = "status_${item_index}" name = "status_${item_index}" class = "multiline-text">${item.status}</textarea>
-        </td>
-    </tr>
-    </#list>
-</table>
-<input type = "hidden" name = "totalItems" id = "totalItems" value = "${totalItems}" /><input id = "responseMessage" title = "${level!""}" style = "display:none" value ='${message!""}'/>
+<!--<#assign totalItems = "0"><#list statuses as item><#assign totalItems = item_index>-->
+<div class = "tweetLine">
+    <div class = "hidden-fields" style ="display:none">
+        <input type = "text" class = "actual-time" id = "updatedTime_${item_index}" name = "updatedTime_${item_index}" value = "${item.time}"/>
+        <input type = "text" name = "key_${item_index}" value = "${item.keyId!""}"/>
+        <input type = "text" name = "source_${item_index}" value = "${item.source}"/>
+    </div>
+    <div class = "left-pane" style ="width:2%">
+        <input type = "checkbox" id = "item_${item_index}" name = "item_${item_index}" class = "item-index" value = "on"/>
+    </div>
+    <div class = "right-pane" style ="width:97%">
+        <textarea id = "status_${item_index}" name = "status_${item_index}" class = "multiline-text">${item.status}</textarea>
+    </div>
+    <div class = "clear bottom-line">
+        <ul class = "left-pane single-line-list">
+            <li class = "screenName" id = "screenName_${item_index}">
+                ${item.twitterScreenName}
+            </li>
+            <li class = "source" id = "source_${item_index}">
+                <span style ="color : #000000">| </span>
+                ${item.source} <span style ="color : #000000">| </span>
+            </li>
+            <li>
+                <input title = "click to edit" class = "time" id = "time_${item_index}">
+                </input>
+            </li>
+            <div class = "left-pane">
+            </div>
+        </ul>
+        <div class = "clear">
+            <span class = "length" id = "length_${item_index}"/>
+        </div>
+    </div>
+</div><!--</#list>-->
+<br/>
+<input type = "hidden" name = "totalItems" id = "totalItems" value = "${totalItems}" />
+<input id = "responseMessage" title = "${level!""}" style = "display:none" value ='${message!""}'/>
