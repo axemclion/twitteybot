b247ed69433f5d023d33be97f54d2c091c25c501
n.parashuram <axemclion@panarasi-laptop>
2010-03-06 11:19:50 +0530

    Added Admin FTL Changed logic in Admin Servlet

--- src/com/appspot/twitteybot/admin/AdminServlet.java
+++ src/com/appspot/twitteybot/admin/AdminServlet.java
@@ -1,6 +1,8 @@
 package com.appspot.twitteybot.admin;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.jdo.PersistenceManager;
 import javax.servlet.ServletException;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.appspot.twitteybot.datastore.ApplicationProperty;
 import com.appspot.twitteybot.datastore.PMF;
+import com.appspot.twitteybot.ui.FreeMarkerConfiguration;
 import com.appspot.twitteybot.ui.Pages;
 
 public class AdminServlet extends HttpServlet {
@@ -17,20 +20,23 @@ public class AdminServlet extends HttpServlet {
 	private static final long serialVersionUID = 6405416403272879573L;
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String action = req.getParameter(Pages.PARAM_ACTION);
 		if (action == null) {
 
 		} else if (action.equalsIgnoreCase(Pages.PARAM_ACTION_ADD)) {
 			PersistenceManager pm = PMF.get().getPersistenceManager();
-			ApplicationProperty prop = new ApplicationProperty(req
-					.getParameter(Pages.PARAM_KEY), req
+			ApplicationProperty prop = new ApplicationProperty(req.getParameter(Pages.PARAM_KEY), req
 					.getParameter(Pages.PARAM_VALUE));
 			pm.makePersistent(prop);
 			pm.close();
 		} else {
-			resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+			String consumerKey = ApplicationProperty.read(ApplicationProperty.CONSUMER_KEY);
+			String consumerSecret = ApplicationProperty.read(ApplicationProperty.CONSUMER_SECRET);
+			Map<String, Object> templateValues = new HashMap<String, Object>();
+			templateValues.put(ApplicationProperty.CONSUMER_KEY, consumerKey);
+			templateValues.put(ApplicationProperty.CONSUMER_SECRET, consumerSecret);
+			FreeMarkerConfiguration.writeResponse(templateValues, Pages.TEMPLATE_ADMINPAGE, resp.getWriter());
 		}
 	}
 }
